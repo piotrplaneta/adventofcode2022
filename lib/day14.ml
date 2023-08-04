cbbdcaa372455e62820efb6ff5b255a8a51ebfaf
piotrplaneta <piotr.planeta@gmail.com>
2023-08-04 23:28:18 +0200

    Add day14 solution

--- lib/day14.ml
+++ lib/day14.ml
@@ -1,6 +1,9 @@
 open Base
 
-let input = [ "498,4 -> 498,6 -> 496,6"; "503,4 -> 502,4 -> 502,9 -> 494,9" ]
+let test_input =
+  [ "498,4 -> 498,6 -> 496,6"; "503,4 -> 502,4 -> 502,9 -> 494,9" ]
+
+let input = Files.read_lines "lib/day14.input"
 
 module IntPair = struct
   module T = struct
@@ -21,11 +24,11 @@ let rec generate_horizontal_line x1 x2 y =
     | _ -> (x1, y) :: generate_horizontal_line (x1 + 1) x2 y
 
 let rec generate_vertical_line y1 y2 x =
-  if y1 > y2 then generate_horizontal_line y2 y1 x
+  if y1 > y2 then generate_vertical_line y2 y1 x
   else
     match y2 - y1 with
     | 0 -> [ (x, y1) ]
-    | _ -> (x, y1) :: generate_horizontal_line (y1 + 1) y2 x
+    | _ -> (x, y1) :: generate_vertical_line (y1 + 1) y2 x
 
 let rec draw_lines points =
   match points with
@@ -46,3 +49,43 @@ let parsed_input =
              |> fun point_list ->
              (List.hd_exn point_list, List.hd_exn (List.tl_exn point_list)))
            points)
+
+let lines = List.map ~f:draw_lines parsed_input
+let flattened_lines = List.concat (List.concat lines)
+let rock_grid = Set.of_list (module IntPair) flattened_lines
+
+let max_rock_y =
+  rock_grid |> Set.to_list
+  |> List.map ~f:(fun (_, y) -> y)
+  |> List.fold ~f:max ~init:0
+
+let rec drop_sand_part_one (from_x, from_y) grid =
+  match Set.mem grid (from_x, from_y + 1) with
+  | _ when from_y + 1 > max_rock_y -> Set.length grid
+  | false -> drop_sand_part_one (from_x, from_y + 1) grid
+  | true when not (Set.mem grid (from_x - 1, from_y + 1)) ->
+      drop_sand_part_one (from_x - 1, from_y + 1) grid
+  | true when not (Set.mem grid (from_x + 1, from_y + 1)) ->
+      drop_sand_part_one (from_x + 1, from_y + 1) grid
+  | true -> drop_sand_part_one (500, 0) (Set.add grid (from_x, from_y))
+
+let part_one = drop_sand_part_one (500, 0) rock_grid - Set.length rock_grid
+
+let rec drop_sand_part_two (from_x, from_y) grid =
+  match Set.mem grid (from_x, from_y) with
+  | true when from_x = 500 && from_y = 0 -> Set.length grid
+  | true -> failwith "Incorrect state of falling"
+  | false
+    when (not (Set.mem grid (from_x, from_y + 1))) && from_y < max_rock_y + 1 ->
+      drop_sand_part_two (from_x, from_y + 1) grid
+  | false
+    when (not (Set.mem grid (from_x - 1, from_y + 1)))
+         && from_y < max_rock_y + 1 ->
+      drop_sand_part_two (from_x - 1, from_y + 1) grid
+  | false
+    when (not (Set.mem grid (from_x + 1, from_y + 1)))
+         && from_y < max_rock_y + 1 ->
+      drop_sand_part_two (from_x + 1, from_y + 1) grid
+  | false -> drop_sand_part_two (500, 0) (Set.add grid (from_x, from_y))
+
+let part_two = drop_sand_part_two (500, 0) rock_grid - Set.length rock_grid
